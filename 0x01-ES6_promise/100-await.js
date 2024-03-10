632b89407a145468d6e705321deec6f7dd00d5cf
kelvin <kelvin4stunt@gmail.com>
2024-03-10 10:20:48 +0100

    up

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,20 +1,21 @@
 import { uploadPhoto, createUser } from './utils.js';
+
 export default async function asyncUploadUser() {
   try {
     const photoPromise = uploadPhoto();
     const userPromise = createUser();
 
     const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
-    
+
     return {
       photo: photoResponse,
-      user: userResponse
+      user: userResponse,
     };
   } catch (error) {
-      console.error(error);
-      return {
-        photo: null,
-	user: null
-      };
+    console.error(error);
+    return {
+      photo: null,
+      user: null,
+    };
   }
 }
