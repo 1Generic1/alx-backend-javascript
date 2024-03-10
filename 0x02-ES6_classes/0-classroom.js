d6ad2867709fe03f776e7607d4a272c05af1ed75
kelvin <kelvin4stunt@gmail.com>
2024-03-10 21:58:34 +0100

    up

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,9 +2,11 @@ export default class ClassRoom {
   constructor(maxStudentSize) {
     this._maxStudentsSize = maxStudentSize;
   }
+
   get maxStudentsSize() {
     return this._maxStudentsSize;
   }
+
   set maxStudentsSize(newMaxStudentsSize) {
     this._maxStudentsSize = newMaxStudentsSize;
   }
