7bbf37d7753950b0986404b07efd56751feccf32
kelvin <kelvin4stunt@gmail.com>
2024-03-11 07:55:09 +0100

    npm

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
 import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
   const rooms = [];
   const sizes = [19, 20, 34];
-  sizes.forEach(size => {
+  sizes.forEach((size) => {
     const room = new ClassRoom(size);
     rooms.push(room);
   });
